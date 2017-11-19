9cada81922f4ace9a6c2b07dc50ac8f5a42a234a
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-19 14:28:07 -0500

    added table to display search results

--- test/d3.js
+++ test/d3.js
@@ -1,15 +1,15 @@
 var apiKey = "NpjePSNP3ovrBFufOHvGfsX43BDQrce4HkWHoTyi";
 var baseUrl = "https://api.propublica.org/congress/v1"
 var congressNum = "/115";
-var chamber = ["/house", "/senate"];
+var chambers = ["house", "senate"];
 
-// GET https://api.propublica.org/congress/v1/members/{member-id}.json
+var searchOut = [];
 
 // using name1 and name2 in case the user reverses the first and last in the search box
 function getMem(cham, name1, name2) {
   // Ajax call to get the data
   $.ajax({
-     url: baseUrl + congressNum + cham + "/members.json",
+     url: baseUrl + congressNum + "/" + cham + "/members.json",
      type: "GET",
      dataType: 'json',
      headers: {'X-API-Key': apiKey}
@@ -18,7 +18,25 @@ function getMem(cham, name1, name2) {
     // Search parameters
     for (var i = 0; i < res.length; i++) {
       if (res[i].first_name === name1 || res[i].last_name === name1 || res[i].first_name === name2 || res[i].last_name === name2) {
-        console.log(res[i]);
+        // Display search results
+        var row = $('<tr/>');
+        var name = $('<td/>');
+        var state = $('<td/>');
+        var chamber = $('<td/>');
+
+        name.text(res[i].first_name + " " + res[i].last_name);
+        name.attr({
+          class: "name",
+          "data-id": res[i].id
+        });
+        state.text(res[i].state);
+        chamber.text(cham[0].toUpperCase() + cham.slice(1));
+
+        row.append(name);
+        row.append(state);
+        row.append(chamber);
+
+        $('table').append(row);
       };
     };
   });
@@ -35,12 +53,6 @@ function detailMem(memNum) {
   });
 }
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-var testNum = "H000338";
-
 // Get a list of members
 $(document).ready(function() {
   $('#searchQuery').on('click', function(event) {
@@ -48,12 +60,12 @@ $(document).ready(function() {
 
     var query = $('#searchText').val().split(" ");
     var fixQ = [];
+    // format the search function for proPublica
     query.forEach(function(element) {
       fixQ.push(element[0].toUpperCase() + element.slice(1));
     });
 
-    console.log(fixQ);
-    getMem(chamber[0], fixQ[0], fixQ[1]);
-    getMem(chamber[1], fixQ[0], fixQ[1]);
+    getMem(chambers[0], fixQ[0], fixQ[1]);
+    getMem(chambers[1], fixQ[0], fixQ[1]);
   });
 })
