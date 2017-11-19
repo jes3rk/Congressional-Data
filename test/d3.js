d6ebdac00dfafea15e7afe99aff21d780bd576fd
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-19 14:07:12 -0500

    search engine works

--- test/d3.js
+++ test/d3.js
@@ -6,10 +6,10 @@ var chamber = ["/house", "/senate"];
 // GET https://api.propublica.org/congress/v1/members/{member-id}.json
 
 // using name1 and name2 in case the user reverses the first and last in the search box
-function getMem(index, name1, name2) {
+function getMem(cham, name1, name2) {
   // Ajax call to get the data
   $.ajax({
-     url: baseUrl + congressNum + chamber[index] + "/members.json",
+     url: baseUrl + congressNum + cham + "/members.json",
      type: "GET",
      dataType: 'json',
      headers: {'X-API-Key': apiKey}
@@ -35,11 +35,25 @@ function detailMem(memNum) {
   });
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 var testNum = "H000338";
 
 // Get a list of members
 $(document).ready(function() {
+  $('#searchQuery').on('click', function(event) {
+    event.preventDefaults;
 
+    var query = $('#searchText').val().split(" ");
+    var fixQ = [];
+    query.forEach(function(element) {
+      fixQ.push(element[0].toUpperCase() + element.slice(1));
+    });
 
-  detailMem(testNum);
+    console.log(fixQ);
+    getMem(chamber[0], fixQ[0], fixQ[1]);
+    getMem(chamber[1], fixQ[0], fixQ[1]);
+  });
 })
