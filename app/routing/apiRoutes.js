682f0035ccf354b91c5308592bb7344eccd34b9d
Le Vu Hoang <rainyhoang@gmail.com>
2017-11-20 14:46:31 -0500

    my stuff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,71 @@ var apiKey = require("../data/keys.js");
 var baseUrl = "https://api.propublica.org/congress/v1"
 var congressNum = "/115";
 var chambers = ["house", "senate"];
+var path = require("path");
+var request = require("request");
+
+
+module.exports = function(app) {
+
+  console.log('in here')
+//==================================Get 1st info===========================
+  app.get('/api/congress', function(req,res){
+
+    getMem('mark','mark','mark')
+    res.json()
+
+
+  })
+
+  function getMem(cham, name1, name2) {
+
+      var queryUrl = baseUrl + congressNum + "/" + cham + "/members.json";
+      request(queryUrl, function(error, response, body) {
+        if(!error && response.statusCode ===200){
+          var res = data.result[0].members;
+          for (i = 0; i < data.reasult[0].members; i++){
+            if(res[i].first_name === name1 || res[i].last_name === name2) {
+              console.log(res);
+            }
+          }
+        }
+      });
+
+      console.log(JSON.parse(body));
+  }
+
+//==================================Function of getting Detail Memeber============================
+  function detailMem(memNum) {
+    $.ajax({
+      url: baseUrl + "/members/" + memNum + ".json",
+      type: "GET",
+      dataType: "json",
+      headers: {'X-API-Key': apiKey}
+    }).done(function(data) {
+      // console.log(data);
+      var basic = data.results[0];
+      // Cleaning the data to make life easier and lower load times
+      memDetail = {
+        name: basic.first_name + " " + basic.last_name,
+        congressNum: basic.member_id,
+        photo: "https://theunitedstates.io/images/congress/225x275/" + basic.member_id + ".jpg",
+        party: basic.current_party,
+        gender: basic.gender,
+        dob: basic.date_of_birth,
+        title: basic.roles[0].short_title,
+        roles: basic.roles,
+        contact: {
+          facebook: "https://www.facebook.com/" + basic.facebook_account,
+          twiiter: "https://www.twitter.com/" + basic.twitter_account,
+          website: basic.url,
+          phone: basic.roles[0].phone
+        }
+      };
+
+      console.log(memDetail);
+    });
+  }
+//==============================List of Memeber==============================
+
+
+}//Module export
