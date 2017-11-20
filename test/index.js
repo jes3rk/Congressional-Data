e9c61152c8f0ff4e9d413e8e37d2f616e55283fc
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-20 14:47:48 -0500

    testing D3

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var randNum = [];
+
+while (randNum.length < 1000) {
+  randNum.push(Math.floor(Math.random() * 100));
+};
+
+console.log(randNum);
+
+// https://bl.ocks.org/d3noob/96b74d0bd6d11427dd797892551a103c
