5c075aaee4d45b6a690ec4705f02965ab43c2bc8
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-25 13:44:02 -0500

    changed server to true

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Make bodyParser work
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/app/public')));
 // Search for and find the routes
