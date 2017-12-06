3599ba017e8a96720c28357c1c4d2616b3b21192
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-06 12:59:55 -0500

    Started ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Congressional-Data
+What are your Representatives in Congress up to? Using this simple tool, you can get useful information about every currently sitting member of Congress. Currently, the page displays contact information, bill sponsorship/cosponsorship, how often they vote with their party, and how often they miss votes.
+
+## Dependencies
+There are a number of dependencies required to run this app locally. All can be installed with ```npm install```. To install manually, you need to add the following packages:
+* ```body-parser```
+* ```express```
+* ```express-handlebars```
+* ```path```
+* ```request```
+
+Additional external sources linked in the html are [D3.js](https://d3js.org/), [Materialize](http://materializecss.com/), and [jQuery](https://code.jquery.com/).
--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,5 +1,53 @@
 var details;
 
+function search() {
+  var memID;
+  event.preventDefault();
+  // Grab the user text
+  var userText = $('#search').val().split(" ");
+  var congressSearch = [];
+  // Format the text according to ProPublica
+  userText.forEach(function(element) {
+    congressSearch.push(element[0].toUpperCase() + element.slice(1));
+  });
+  // Log for QA
+  console.log(congressSearch);
+  // API call to the server
+  $.post("/api/search", {object: congressSearch})
+    .done(function(data) {
+      // Check the length of the data returned
+      if (data.length === 1) {
+        // Redirect to the member results page based on the memebr
+        $(location).attr('href', '/congress/member/' + data[0].id);
+      } else {
+        // Generates a table for users to select the correct result
+        // Shouldn't be needed
+        for (var i = 0; i < data.length; i++) {
+          var row = $('<tr/>');
+
+          var name = $('<td/>');
+          name.text(data[i].name);
+          name.attr({
+            "data-id": data[i].id,
+            "class": "pick"
+          });
+
+          var state = $('<td/>');
+          state.text(data[i].state);
+
+          row.append(name);
+          row.append(state);
+
+          $('.choose').append(row);
+        };
+        // Redirect from the table of results
+        $('td').on('click touch', function() {
+          $(location).attr('href', '/congress/member/' + $(this).attr("data-id"));
+        });
+    };
+  });
+};
+
 function memberReady() {
   $('.collapsible').collapsible();
 
@@ -34,50 +82,12 @@ $(document).ready(function() {
   $('select').material_select();
 
   $('#searchGo').on('click touch', function() {
-    var memID;
-    event.preventDefault();
-    // Grab the user text
-    var userText = $('#search').val().split(" ");
-    var congressSearch = [];
-    // Format the text according to ProPublica
-		userText.forEach(function(element) {
-			congressSearch.push(element[0].toUpperCase() + element.slice(1));
-		});
-    // Log for QA
-		console.log(congressSearch);
-    // API call to the server
-    $.post("/api/search", {object: congressSearch})
-      .done(function(data) {
-        // Check the length of the data returned
-        if (data.length === 1) {
-          // Redirect to the member results page based on the memebr
-          $(location).attr('href', '/congress/member/' + data[0].id);
-        } else {
-          // Generates a table for users to select the correct result
-          // Shouldn't be needed
-          for (var i = 0; i < data.length; i++) {
-            var row = $('<tr/>');
+    search();
+  });
 
-            var name = $('<td/>');
-            name.text(data[i].name);
-            name.attr({
-              "data-id": data[i].id,
-              "class": "pick"
-            });
-
-            var state = $('<td/>');
-            state.text(data[i].state);
-
-            row.append(name);
-            row.append(state);
-
-            $('.choose').append(row);
-          };
-          // Redirect from the table of results
-          $('td').on('click touch', function() {
-            $(location).attr('href', '/congress/member/' + $(this).attr("data-id"));
-          });
-      };
-    });
+  $(document).keypress(function (e) {
+    if (e.which == 13) {
+      search();
+    };
   });
 });
